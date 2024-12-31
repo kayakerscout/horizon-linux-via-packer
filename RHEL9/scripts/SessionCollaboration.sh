84330222b2498df987b06a760a210b1e8d018162
Mike Renfro <mike+github@renf.ro>
2024-12-31 16:50:20 -0600

    reduce copy/pasted text

--- RHEL9/scripts/SessionCollaboration.sh
+++ RHEL9/scripts/SessionCollaboration.sh
@@ -2,10 +2,10 @@
 #https://access.redhat.com/documentation/en-us/red_hat_enterprise_linux/9/html/customizing_the_gnome_desktop_environment/enabling-and-enforcing-gnome-shell-extensions_customizing-the-gnome-desktop-environment
 #https://docs.omnissa.com/bundle/Desktops-and-Applications-in-HorizonV2406/page/ConfiguringSessionCollaborationonLinuxDesktops.html
 cd /tmp/PackerBuildFiles/SoftwareDownloads/
-mkdir "appindicatorsupport@rgcjonas.gmail.com"
-unzip ./appindicatorsupportrgcjonas.gmail.com.v42.shell-extension.zip -d "appindicatorsupport@rgcjonas.gmail.com/"
-mv "appindicatorsupport@rgcjonas.gmail.com" "/usr/share/gnome-shell/extensions/appindicatorsupport@rgcjonas.gmail.com"
-chmod -R 755 "/usr/share/gnome-shell/extensions/appindicatorsupport@rgcjonas.gmail.com/"
+base_name="appindicatorsupport@rgcjonas.gmail.com"
+unzip ./${base_name}.v42.shell-extension.zip -d "${base_name}/"
+mv "${base_name}" "/usr/share/gnome-shell/extensions"
+chmod -R 755 "/usr/share/gnome-shell/extensions/${base_name}/"
 cd /tmp/PackerBuildFiles/OtherFiles/
 cp ./00-extensions.txt /etc/dconf/db/local.d/00-extensions
 chmod 744 /etc/dconf/db/local.d/00-extensions
